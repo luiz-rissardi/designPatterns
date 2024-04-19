448d5f507a577d52b6854ef80f2c909bab64e911
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-04-18 22:29:21 -0300

    ADD:only gravation video, and play recorder video audio with video

--- teste/index.js
+++ teste/index.js
@@ -6,16 +6,20 @@ const btnPause = document.getElementById("pause")
 const btnPlay = document.getElementById("play")
 const videoPlayer = document.getElementById("videoElement")
 
-btnPause.addEventListener("click",stopGravation)
-btnPlay.addEventListener("click",playGravation)
-btnStart.addEventListener("click",startGravation)
+btnPause.addEventListener("click", stopGravation)
+btnPlay.addEventListener("click", playGravation)
+btnStart.addEventListener("click", startGravation)
 
 async function startGravation() {
     dataStreamRecorder.length = 0
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio:true });
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
     mediaRecorder = new MediaRecorder(stream);
     mediaRecorder.ondataavailable = handlerDataStream;
-    videoPlayer.srcObject = stream
+
+    const videoTracks = stream.getVideoTracks();
+    const mediaVideoRecorder = new MediaStream(videoTracks);
+    videoPlayer.srcObject = mediaVideoRecorder;
+
     mediaRecorder.start();
 }
 
@@ -27,12 +31,19 @@ async function stopGravation() {
     mediaRecorder.stop();
 }
 
-function playGravation(){
-    videoPlayer.srcObject = null
-    const videoBlob = new Blob(dataStreamRecorder,{
-        type:"video"
+function playGravation() {
+    videoPlayer.srcObject = null;
+
+    // Filtrar apenas os dados de vídeo
+    const videoBlobs = dataStreamRecorder.filter(data => {
+        return data.type.startsWith('video')
     });
+
+    // Criar um novo Blob apenas com os dados de vídeo
+    const videoBlob = new Blob(videoBlobs, { type: 'video/webm' });
+
+    // Criar uma URL para o Blob de vídeo e atribuir ao elemento de vídeo
     const videoUrl = URL.createObjectURL(videoBlob);
     videoPlayer.src = videoUrl;
-    videoPlayer.controls = true
+    videoPlayer.controls = true;
 }
